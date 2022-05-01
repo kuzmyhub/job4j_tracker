c3cb894d734497bb93a7220a805192dfbe742352
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-05-01 08:03:23 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,9 +1,37 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
+
+    public Item[] findByName(String key) {
+        Item[] itemsMatchingNames = new Item[size];
+        int sizeMatchingNames = 0;
+        for (int i = 0; i < itemsMatchingNames.length; i++) {
+            if (key.equals(items[i].getName())) {
+                itemsMatchingNames[sizeMatchingNames] = items[i];
+                sizeMatchingNames++;
+            }
+        }
+        itemsMatchingNames = Arrays.copyOf(itemsMatchingNames, sizeMatchingNames);
+        return itemsMatchingNames;
+    }
+
+    public Item[] findAll(Item[] items) {
+        Item[] itemsWithoutNull = new Item[size];
+        int sizeWithoutNull = 0;
+        for (int i = 0; i < itemsWithoutNull.length; i++) {
+            if (items[i] != null) {
+                itemsWithoutNull[sizeWithoutNull] = items[i];
+                sizeWithoutNull++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, sizeWithoutNull);
+        return itemsWithoutNull;
+    }
 
     public Item add(Item item) {
         item.setId(ids++);
@@ -21,5 +49,9 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] getItems() {
+        return items;
     }
 }
