ce30e43bde38167c1f5e5731486690ce906d0130
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-05-02 19:46:31 +0300

    5. Tracker - хранилище [#396 #295730]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,14 +57,6 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] result = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                result[count] = items[i];
-                count++;
-            }
-        }
         return Arrays.copyOf(items, size);
     }
 
