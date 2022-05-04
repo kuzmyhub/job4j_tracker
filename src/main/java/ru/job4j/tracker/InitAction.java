9aa843d78449911dde1aa0f2206fc63ac4288358
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-05-04 21:18:55 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/InitAction.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker;
+
+public class InitAction implements UserAction {
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            int select = input.askInt("Select: ");
+            if (select == 0) {
+                StartUI.createItem(input, tracker);
+            } else if (select == 1) {
+                StartUI.showItems(tracker);
+            } else if (select == 2) {
+                StartUI.editItem(input, tracker);
+            } else if (select == 3) {
+                StartUI.deleteItem(input, tracker);
+            } else if (select == 4) {
+                StartUI.findItemById(input, tracker);
+            } else if (select == 5) {
+                StartUI.findItemsByName(input, tracker);
+            } else if (select == 6) {
+                run = false;
+            }
+        }
+        return true;
+    }
+}
