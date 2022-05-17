bbcb2b29dce6c0fd06ab246385e47bdcdaea6cf9
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-05-17 19:06:15 +0300

    2. Комбинированный компаратор [#10036 #301868]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -76,8 +76,8 @@ public class JobTest {
                 .thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 1)
         );
-        assertThat(rsl, lessThan(0));
+        assertThat(rsl, greaterThan(0));
     }
 }
