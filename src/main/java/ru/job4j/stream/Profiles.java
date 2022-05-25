d5e1308b036d7a87729e408cdf0e4522e62681e2
Kuznetsov S. I <kuznetsov.s.i@bk.ru>
2022-05-25 19:19:21 +0300

    2. Список адресов [#110062 #304933]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream().
-                map(profile -> profile.getAddress()).
+                map(Profile::getAddress).
                 collect(Collectors.toList());
     }
 
