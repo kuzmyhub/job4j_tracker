69ff70e9973fc4d0b6bf480497f21b0bf0ce2e9c
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-05-25 07:34:57 +0300

    6. Функции высшего порядка [#24255 #304585]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -13,18 +13,10 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        Comparator<Person> compareName = Comparator.comparing(Person::getName);
-        Comparator<Person> compareSurname = Comparator.comparing(Person::getSurname);
-        Comparator<Person> comparePhone = Comparator.comparing(Person::getPhone);
-        Comparator<Person> compareAddress = Comparator.comparing(Person::getAddress);
-        Comparator<Person> cmpCombine = compareName.
-                thenComparing(compareSurname).
-                thenComparing(comparePhone).
-                thenComparing(compareAddress);
-        Predicate<Person> nameContains = n -> n.getName().contains(key);
-        Predicate<Person> surnameContains = s -> s.getSurname().contains(key);
+        Predicate<Person> nameContains = p -> p.getName().contains(key);
+        Predicate<Person> surnameContains = p -> p.getSurname().contains(key);
         Predicate<Person> phoneContains = p -> p.getPhone().contains(key);
-        Predicate<Person> addressContains = a -> a.getAddress().contains(key);
+        Predicate<Person> addressContains = p -> p.getAddress().contains(key);
         Predicate<Person> prCombine = nameContains.
                 or(surnameContains.
                         or(phoneContains.
@@ -34,7 +26,6 @@ public class PhoneDictionary {
                 result.add(value);
             }
         }
-        result.sort(cmpCombine);
         return result;
     }
 }
