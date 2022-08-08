267bc3c2eae9e6d2b24e560d34801f9a5b1ad4e8
Sergey <kuznetsov.s.i@bk.ru>
2022-08-08 09:26:00 +0300

    3. Liquibase. Интеграционные тесты. [#95881]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -7,16 +7,13 @@ import org.junit.Test;
 import ru.job4j.tracker.Item;
 
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class SqlTrackerTest {
 
@@ -52,10 +49,119 @@ public class SqlTrackerTest {
     }
 
     @Test
+    public void whenSaveItemThenResultSetMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        Item testItem = null;
+        try (PreparedStatement statement
+                     = connection.prepareStatement(
+                             "select id, name, created from items;"
+        )) {
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            testItem = tracker.getItem(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        assertThat(item, is(testItem));
+    }
+
+    @Test
     public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
         Item item = new Item("item");
         tracker.add(item);
         assertThat(tracker.findById(item.getId()), is(item));
+    }
+
+    @Test
+    public void whenSaveItemAndFindByNameThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item firstItem = new Item("item");
+        tracker.add(firstItem);
+        Item secondItem = new Item("item");
+        tracker.add(secondItem);
+        Item thirdItem = new Item("otherItem");
+        tracker.add(thirdItem);
+        Item fourthItem = new Item("item");
+        tracker.add(fourthItem);
+        List<Item> items = List.of(
+                new Item(firstItem.getId(), "item"),
+                new Item(secondItem.getId(), "item"),
+                new Item(fourthItem.getId(), "item")
+        );
+        assertThat(tracker.findByName("item"), is(items));
+    }
+
+    @Test
+    public void whenSaveItemAndFindAllThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item firstItem = new Item("item");
+        tracker.add(firstItem);
+        Item secondItem = new Item("item");
+        tracker.add(secondItem);
+        Item thirdItem = new Item("otherItem");
+        tracker.add(thirdItem);
+        Item fourthItem = new Item("item");
+        tracker.add(fourthItem);
+        List<Item> items = List.of(
+                new Item(firstItem.getId(), "item"),
+                new Item(secondItem.getId(), "item"),
+                new Item(thirdItem.getId(), "otherItem"),
+                new Item(fourthItem.getId(), "item")
+        );
+        assertThat(tracker.findAll(), is(items));
+    }
+
+    @Test
+    public void whenSaveItemAndDeleteThenTrue() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertTrue(tracker.delete(item.getId()));
+    }
+
+    @Test
+    public void whenSaveItemAndDeleteThenFalse() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertFalse(tracker.delete(0));
+    }
+
+    @Test
+    public void whenSaveItemAdDeleteThenItemNotFound() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        tracker.delete(item.getId());
+        assertThat(tracker.findByName("item"), is(List.of()));
+    }
+
+    @Test
+    public void whenSaveItemAndReplaceThenTrue() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertTrue(tracker.replace(item.getId(), new Item("testItem")));
+    }
+
+    @Test
+    public void whenSaveItemAndReplaceThenFalse() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertFalse(tracker.replace(0, new Item("testItem")));
+    }
+
+    @Test
+    public void whenSaveItemAndReplaceThenFindReplacedItem() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        tracker.replace(item.getId(), new Item("testItem"));
+        assertThat(tracker.findByName("testItem"),
+                is(List.of(new Item(item.getId(), "testItem"))));
     }
 }
