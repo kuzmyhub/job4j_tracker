2e78d726d5d0a3e02bda4788a677a2940ab5994d
Sergey <kuznetsov.s.i@bk.ru>
2022-08-11 16:46:24 +0300

    3. Liquibase. Интеграционные тесты. [#95881 #329937]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -49,119 +49,97 @@ public class SqlTrackerTest {
     }
 
     @Test
-    public void whenSaveItemThenResultSetMustBeTheSame() {
-        SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
-        Item testItem = null;
-        try (PreparedStatement statement
-                     = connection.prepareStatement(
-                             "select id, name, created from items;"
-        )) {
-            ResultSet resultSet = statement.executeQuery();
-            resultSet.next();
-            testItem = tracker.getItem(resultSet);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        assertThat(item, is(testItem));
-    }
-
-    @Test
     public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item"));
         assertThat(tracker.findById(item.getId()), is(item));
     }
 
     @Test
     public void whenSaveItemAndFindByNameThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item firstItem = new Item("item");
-        tracker.add(firstItem);
-        Item secondItem = new Item("item");
-        tracker.add(secondItem);
-        Item thirdItem = new Item("otherItem");
-        tracker.add(thirdItem);
-        Item fourthItem = new Item("item");
-        tracker.add(fourthItem);
-        List<Item> items = List.of(
+        Item firstItem = tracker.add(new Item("item"));
+        Item secondItem = tracker.add(new Item("item"));
+        Item thirdItem = tracker.add(new Item("otherItem"));
+        Item fourthItem = tracker.add(new Item("item"));
+        assertThat(tracker.findByName("item"), is(List.of(
                 new Item(firstItem.getId(), "item"),
                 new Item(secondItem.getId(), "item"),
-                new Item(fourthItem.getId(), "item")
-        );
-        assertThat(tracker.findByName("item"), is(items));
+                new Item(fourthItem.getId(), "item"))));
     }
 
     @Test
     public void whenSaveItemAndFindAllThenMustBeTheSame() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item firstItem = new Item("item");
-        tracker.add(firstItem);
-        Item secondItem = new Item("item");
-        tracker.add(secondItem);
-        Item thirdItem = new Item("otherItem");
-        tracker.add(thirdItem);
-        Item fourthItem = new Item("item");
-        tracker.add(fourthItem);
-        List<Item> items = List.of(
+        Item firstItem = tracker.add(new Item("item"));
+        Item secondItem = tracker.add(new Item("item"));
+        Item thirdItem = tracker.add(new Item("otherItem"));
+        Item fourthItem = tracker.add(new Item("item"));
+        assertThat(tracker.findAll(), is(List.of(
                 new Item(firstItem.getId(), "item"),
                 new Item(secondItem.getId(), "item"),
                 new Item(thirdItem.getId(), "otherItem"),
-                new Item(fourthItem.getId(), "item")
-        );
-        assertThat(tracker.findAll(), is(items));
+                new Item(fourthItem.getId(), "item"))));
     }
 
     @Test
     public void whenSaveItemAndDeleteThenTrue() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item"));
         assertTrue(tracker.delete(item.getId()));
     }
 
     @Test
     public void whenSaveItemAndDeleteThenFalse() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item"));
         assertFalse(tracker.delete(0));
     }
 
     @Test
     public void whenSaveItemAdDeleteThenItemNotFound() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item"));
         tracker.delete(item.getId());
         assertThat(tracker.findByName("item"), is(List.of()));
     }
 
     @Test
+    public void whenSaveItemAdDeleteThenNull() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = tracker.add(new Item("item"));
+        tracker.delete(item.getId());
+        assertNull(tracker.findById(item.getId()));
+    }
+
+    @Test
     public void whenSaveItemAndReplaceThenTrue() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item"));
         assertTrue(tracker.replace(item.getId(), new Item("testItem")));
     }
 
     @Test
     public void whenSaveItemAndReplaceThenFalse() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add(new Item("item"));
         assertFalse(tracker.replace(0, new Item("testItem")));
     }
 
     @Test
     public void whenSaveItemAndReplaceThenFindReplacedItem() {
         SqlTracker tracker = new SqlTracker(connection);
-        Item item = new Item("item");
-        tracker.add(item);
+        Item item = tracker.add( new Item("item"));
         tracker.replace(item.getId(), new Item("testItem"));
         assertThat(tracker.findByName("testItem"),
                 is(List.of(new Item(item.getId(), "testItem"))));
+    }
+
+    @Test
+    public void whenSaveItemAndReplaceThenNameIsSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = tracker.add(new Item("item"));
+        tracker.replace(item.getId(), new Item("testItem"));
+        assertThat(tracker.findById(item.getId()).getName(), is("testItem"));
     }
 }
